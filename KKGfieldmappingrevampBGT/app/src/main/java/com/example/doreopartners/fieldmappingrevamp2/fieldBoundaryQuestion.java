fbe7713af9b691aa7246a01a7d08d22664ff88f4
unknown <ayodeji.bakare@babbangona.com>
2019-11-15 14:11:43 +0100

    normal checks

--- KKGfieldmappingrevampBGT/app/src/main/java/com/example/doreopartners/fieldmappingrevamp2/fieldBoundaryQuestion.java
+++ KKGfieldmappingrevampBGT/app/src/main/java/com/example/doreopartners/fieldmappingrevamp2/fieldBoundaryQuestion.java
@@ -9,5 +9,6 @@ public class fieldBoundaryQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_boundary_question);
+
     }
 }
